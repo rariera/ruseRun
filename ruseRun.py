c11a4e665a1841c4c8d2ac4c69f352ace068e8d7
Gavin Carr <gavin@openfusion.com.au>
2017-12-31 15:07:35 +1100

    split files, ditched panels

--- ruseRun.py
+++ ruseRun.py
@@ -2,6 +2,7 @@
 
 from curses import wrapper, panel
 import curses
+from interface import mapinit, charplace, moveChar
 
 class Character(object):
     def __init__(self, cur_y, cur_x):
@@ -13,75 +14,28 @@ character = Character(cur_y = 2, cur_x = 2)
 def main(stdscr):
     '''The main function which will run throughout the game.'''
     curses.initscr()
-    pad1.box()  #a box appears around the window
-    gameMap = '''.............................>>>>>>>>>
-............................>>>>>>>
-......................>>>>>>>
-......................>>>>>>>
-......................
-......................
-......................
-......................
-......................
-............................
-............................
-............................
-............................
-............................'''
-    pad1.addstr(0, 0, gameMap)
-    lrow = 2
-    lcol = 2
-    pminrow = 5
-    pmincol = 5
-    pmaxrow = 30
-    pmaxcol = 15
+    mapinit()
     while True:
-        pad1.addch(character.cur_y, character.cur_x , '@')
-        pad1.refresh(2, 2, 5, 5, 30, 13)
         answer = stdscr.getkey()    #input a key
-        checkAnswer(answer, pad1)
-        pad1.refresh(lrow, lcol, pminrow, pmincol, pmaxrow, pmaxcol)
+        checkAnswer(answer)
         
 
 
 def checkAnswer(answer, pad1):
     '''decides what to do with the input'''
     if answer == 'KEY_UP':
-        moveChar('up', pad1)
+        moveChar('up')
     elif answer == 'KEY_DOWN':
-        moveChar('down', pad1)
+        moveChar('down')
     elif answer == 'KEY_LEFT':
-        moveChar('left', pad1)
+        moveChar('left')
     elif answer == 'KEY_RIGHT':
-        moveChar('right', pad1)
+        moveChar('right')
 
 
         
-def moveChar(direction, pad1):
-    '''Moves the character symbol in accordance with the direction.'''
-    if direction == 'up':
-        pad1.addch(character.cur_y, character.cur_x, '.') 
-        character.cur_y -= 1
-    elif direction == 'down':
-        pad1.addch(character.cur_y, character.cur_x, '.')
-        character.cur_y += 1
-    elif direction == 'left':
-        pad1.addch(character.cur_y, character.cur_x, '.')
-        character.cur_x -= 1
-    elif direction == 'right':
-        pad1.addch(character.cur_y, character.cur_x, '.')
-        character.cur_x += 1
-        
-def panelinit():
-    pad1 = curses.newpad(40, 40) #creating a window that is 40x40
-    back_panel = panel.new_panel(window1)   #make a panel for the first window
-    back_panel.top()
-    window2 = curses.newwin(15, 30, 1, 1)   #creating a second window
-    middle_panel = panel.new_panel(window2)
-    window3 = curses.newwin(15, 30, 1, 1)
-    front_panel = panel.new_panel(window3)
-    window1.box()  #a box appears around the window
-    panel.update_panels()
-    doupdate()
 
+        
+
+    
 wrapper(main)
