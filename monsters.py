df1d91d19f12d224a917714111870af638fc5af2
Laura Wrigley-Carr <rariera02@gmail.com>
2018-04-18 13:14:01 +1000

    worked on monster movement

--- monsters.py
+++ monsters.py
@@ -28,44 +28,60 @@ zeroItems = []
 
 def moveChoose(monsterlist, closest_y, closest_x, character):
     monster = monsterlist[0]
-    monstery = abs(monster.y_coord - closest_y)
-    monsterx = abs(monster.x_coord - closest_x)
-    if monstery < monsterx and monstery != 1:
-        if monster.y_coord - closest_y < 0:
-            moveMonster(monsterlist, 'up')
-        else:
-            moveMonster(monsterlist, 'down')
-    elif monsterx < monstery and monsterx != 1:
-        if monster.x_coord - closest_x < 0:
-            moveMonster(monsterlist, 'right')
-        else:
-            moveMonster(monsterlist, 'left')
-    elif monstery == 1 and monsterx == 1:
-        if y == screen.location['lrow'] + 6:
-            attackPlayer(character, monster)
-        else:
-            monsPickup(monster, lvls.lvl1((y, x)))
+    monstery = abs(monster.y_coord - closest_y) #number of moves up/down to reach @
+    monsterx = abs(monster.x_coord - closest_x) #number of moves left/right to reach @
+    proximity = monstery + monsterx #proximity is the number of moves to reach @
+    if monstery > 1 or monsterx > 1:
+        if monstery > monsterx:
+            if monster.y_coord - closest_y < 0:
+                moveMonster(monsterlist, 'up')
+            else:
+                moveMonster(monsterlist, 'down')
+        elif monsterx > monstery:
+            if monster.x_coord - closest_x < 0:
+                moveMonster(monsterlist, 'right')
+            else:
+                moveMonster(monsterlist, 'left')
+    #elif monstery == 1 and monsterx == 1:
+     #   if closest_y == screen.location['lrow'] + 6:
+      #      attackPlayer(character, monster)
+       # else:
+        #    monsPickup(monster, lvls.lvl1((y, x)))
 
 def moveMonster(monsterlist, direction):
     monster = monsterlist[0]
     if direction == 'up':
-        monster.y_coord -= 1
-        screen.addChar(screen.pad, monster.y_coord + 1, monster.x_coord, monsterlist[1], rainbow.white)
+        move = monsVerify(monster.y_coord - 1, monster.x_coord)
+        if move == True:
+            monster.y_coord -= 1
+            screen.addChar(screen.pad, monster.y_coord + 1, monster.x_coord, monsterlist[1], rainbow.white)
     elif direction == 'down':
-        monster.y_coord += 1
-        screen.addChar(screen.pad, monster.y_coord - 1, monster.x_coord, monsterlist[1], rainbow.white)
+        move = monsVerify(monster.y_coord + 1, monster.x_coord)
+        if move == True:
+            monster.y_coord += 1
+            screen.addChar(screen.pad, monster.y_coord - 1, monster.x_coord, monsterlist[1], rainbow.white)
     elif direction == 'left':
-        monster.x_coord -= 1
-        screen.addChar(screen.pad, monster.y_coord, monster.x_coord + 1, monsterlist[1], rainbow.white)
+        move = monsVerify(monster.y_coord, monster.x_coord - 1)
+        if move == True:
+            monster.x_coord -= 1
+            screen.addChar(screen.pad, monster.y_coord, monster.x_coord + 1, monsterlist[1], rainbow.white)
     elif direction == 'right':
-        monster.x_coord += 1
-        screen.addChar(screen.pad, monster.y_coord, monster.x_coord - 1, monsterlist[1], rainbow.white)
+        move = monsVerify(monster.y_coord, monster.x_coord + 1)
+        if move == True:
+            monster.x_coord += 1
+            screen.addChar(screen.pad, monster.y_coord, monster.x_coord - 1, monsterlist[1], rainbow.white)
     del monsterlist[1]
     prev = screen.getChar(screen.pad, monster.y_coord, monster.x_coord)
     monsterlist.append(prev[0])
     screen.addChar(screen.pad, monster.y_coord, monster.x_coord, monster.tile, monster.colour)
     screen.padRefresh()
 
+def monsVerify(y_coord, x_coord):
+    place = screen.getChar(screen.pad, y_coord, x_coord)
+    move = True
+    if place[0] == '#':
+        move = False
+    return move
 
 
 def monstersUpdate(character, level_monsters):
@@ -73,18 +89,24 @@ def monstersUpdate(character, level_monsters):
         level = level_monsters.lvl1
     else:
         level = level_monsters.lvl2
+    floorlist = floorList()
+    sampleSize = len(floorlist) / 100
+    zeroItems = sample(floorlist, int(sampleSize))
     for monsterlist in level:
-        floorlist = floorList()
-        sampleSize = len(floorlist) / 75
-        zeroItems = sample(floorlist, int(sampleSize))
-        if screen.location['lrow'] + 6 in range(monsterlist[0].y_coord + 10, monsterlist[0].y_coord - 10) or screen.location['lcol'] + 14 in range(monsterlist[0].x_coord + 10, monsterlist[0].x_coord - 10):
-            moveChoose(monsterlist, screen.location['lrow'] + 6, screen.location['lcol'] + 14, character)
-        else: 
-            closest_item = zeroItems[0]
-            for i in zeroItems:
-                if monsterlist[0].y_coord - closest_item[0] + monsterlist[0].x_coord - closest_item[1] >  monsterlist[0].y_coord - i[0] + monsterlist[0].x_coord - i[1]:
-                    closest_item = i
-            moveChoose(monsterlist, closest_item[0], closest_item[1], character)
+        monster = monsterlist[0]
+        if screen.location['lrow'] + 6 in range(monster.y_coord - 50, monster.y_coord + 50) and screen.location['lcol'] + 14 in range(monster.x_coord - 50, monster.x_coord + 50):
+            screen.addString(screen.wintest, 2, 2, 'monstersUpdate()', rainbow.red)
+            screen.winRefresh(screen.wintest)
+            if screen.location['lrow'] + 6 in range(monster.y_coord - 10, monster.y_coord + 10) or screen.location['lcol'] + 14 in range(monster.x_coord - 10, monster.x_coord + 10):
+                moveChoose(monsterlist, screen.location['lrow'] + 6, screen.location['lcol'] + 14, character)
+            else: 
+                closest_item = zeroItems[0]
+                for i in zeroItems:
+                    if monsterlist[0].y_coord - closest_item[0] + monsterlist[0].x_coord - closest_item[1] >  monsterlist[0].y_coord - i[0] + monsterlist[0].x_coord - i[1]:
+                        closest_item = i
+                moveChoose(monsterlist, closest_item[0], closest_item[1], character)
+    screen.addString(screen.wintest, 4, 2, 'End', rainbow.green)
+    screen.winRefresh(screen.wintest)
 
          
         #make list of x-coords including items + character (character must be differentiated) 
--- movement.py
+++ movement.py
@@ -33,50 +33,32 @@ def verify(character, direction, level_monsters, level_items):
         
 def moveChar(character, direction, level_monsters, level_items):
     '''Moves the character symbol in accordance with the direction.'''
+    attrs = verify(character, direction, level_monsters, level_items)
+    item = attrs[0]
+    levels = attrs[1]
     if direction == 'up':
-        attrs = verify(character, direction, level_monsters, level_items)
-        item = attrs[0]
-        levels = attrs[1]
         if item[0] != '#':
             screen.location['lrow'] -= 1
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 14, '@', rainbow.yellow_bg) 
             screen.addChar(screen.pad, screen.location['lrow'] + 7, screen.location['lcol'] + 14, character.pc[0], character.pc[1])
-            character.pc = item
-            screen.addString(screen.winstatus, 4, 2, 'Level: ' + str(character.level), rainbow.white)
-            screen.addString(screen.winstatus, 5, 2, '(' + str(screen.location['lrow'] + 6) + ', ' + str(screen.location['lcol'] + 14) + ')', rainbow.white)
     elif direction == 'down':
-        attrs = verify(character, direction, level_monsters, level_items)
-        item = attrs[0]
-        levels = attrs[1]
         if item[0] != '#':
             screen.location['lrow'] += 1
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 14, '@', rainbow.yellow_bg)
             screen.addChar(screen.pad, screen.location['lrow'] + 5, screen.location['lcol'] + 14, character.pc[0], character.pc[1])
-            character.pc = item
-            screen.addString(screen.winstatus, 4, 2, 'Level: ' + str(character.level), rainbow.white)
-            screen.addString(screen.winstatus, 5, 2, '(' + str(screen.location['lrow'] + 6) + ', ' + str(screen.location['lcol'] + 14) + ')', rainbow.white)
     elif direction == 'left':
-        attrs = verify(character, direction, level_monsters, level_items)
-        item = attrs[0]
-        levels = attrs[1]
         if item[0] != '#':
             screen.location['lcol'] -= 1
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 14, '@', rainbow.yellow_bg)
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 15, character.pc[0], character.pc[1])
-            character.pc = item
-            screen.addString(screen.winstatus, 4, 2, 'Level: ' + str(character.level), rainbow.white)
-            screen.addString(screen.winstatus, 5, 2, '(' + str(screen.location['lrow'] + 6) + ', ' + str(screen.location['lcol'] + 14) + ')', rainbow.white)
     elif direction == 'right':
-        attrs = verify(character, direction, level_monsters, level_items)
-        item = attrs[0]
-        levels = attrs[1]
         if item[0] != '#':
             screen.location['lcol'] += 1
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 14, '@', rainbow.yellow_bg)
             screen.addChar(screen.pad, screen.location['lrow'] + 6, screen.location['lcol'] + 13, character.pc[0], character.pc[1])
-            character.pc = item
-            screen.addString(screen.winstatus, 4, 2, 'Level: ' + str(character.level), rainbow.white)
-            screen.addString(screen.winstatus, 5, 2, '(' + str(screen.location['lrow'] + 6) + ', ' + str(screen.location['lcol'] + 14) + ')', rainbow.white)
+    character.pc = item
+    screen.addString(screen.winstatus, 4, 2, 'Level: ' + str(character.level), rainbow.white)
+    screen.addString(screen.winstatus, 5, 2, '(' + str(screen.location['lrow'] + 6) + ', ' + str(screen.location['lcol'] + 14) + ')', rainbow.white)
     screen.padRefresh()
     screen.winRefresh(screen.winstatus)
     return levels
